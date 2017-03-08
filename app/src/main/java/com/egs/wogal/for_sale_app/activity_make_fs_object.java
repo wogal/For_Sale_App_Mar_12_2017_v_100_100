51a7fb7e5ff853da4f8eeb16a06fda23a11c6d36
wogal <egilgeous@gmail.com>
2017-03-08 10:32:19 -0500

    commit 2

---
+++ app/src/main/java/com/egs/wogal/for_sale_app/activity_make_fs_object.java
@@ -0,0 +1,13 @@
+package com.egs.wogal.for_sale_app;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class activity_make_fs_object extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_make_fs_object);
+    }
+}
